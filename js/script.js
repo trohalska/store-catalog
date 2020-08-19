64e93530eb17ddaf01a99ab333967031c8fa8047
Tetiana Rohalska <55732200+trohalska@users.noreply.github.com>
2020-08-19 18:39:42 +0300

    filters+sort

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-
-// <div class="qty">quantity</div><input type="text" value="1" class="item_Quantity">
-// <div data-location="/checkout/" class="button">Buy</div>
-
 let crumbs = {
   addCrumb: function(str) {
     let node = document.querySelector('.crumbs');
@@ -33,7 +29,7 @@ let actions = {
     let list;
 
     basket.total = 0;
-    for (let i in basket.shopList) {
+    for (let i = 0; i < basket.shopList.length; ++i) {
       list = basket.shopList[i];
       list.subtotal = list.price * list.qty;
       basket.total += list.subtotal;
@@ -45,22 +41,33 @@ let actions = {
 let view = {
   addToBasket: function() {
     let obj = {},
-        i = event.target.getAttribute('data'),
-        item = products.items[i];
+        code = +event.target.getAttribute('code'),
+        item;
 
-    for (let j = 0; basket.shopList[j]; ++j) {
-      let list = basket.shopList[j];
-      if (item.code === list.code) {
-        list.qty += 1;
-        document.querySelector('.front-cart').innerHTML = '(' + actions.countTotal() + ')';
-        basket.showBasket();
-        return;
+    console.log(code, products.items.length);
+
+    for (let i = 0; i < products.items.length; ++i) {
+      if (products.items[i].code === code) {
+        item = products.items[i];
+        break;
       }
     }
 
+    for (let j = 0; j < basket.shopList.length; ++j) {
+      let list = basket.shopList[j];
+      if (item.code === list.code) {
+        if (list.qty < list.amount) {
+          list.qty += 1;
+          document.querySelector('.front-cart').innerHTML = '(' + actions.countTotal() + ')';
+          basket.showBasket();
+        }
+        return;
+      }
+    }
     obj.code = item.code;
+    obj.amount = item.amountAvailable;
     obj.name = item.name + ' ' + item.brand + ' ' + item.taste + ', '
-               + item.sizeG + 'g';
+                + item.sizeG + 'g';
     obj.price = item.price;
     obj.qty = 1;
     obj.subtotal = item.price;
@@ -71,70 +78,98 @@ let view = {
   openBasket: function() {
     if (basket.total === 0)
       return;
-    document.querySelector('main').setAttribute('class', ' hidden');
     let tmp = document.querySelector('.basket');
     tmp.removeAttribute('class');
     tmp.setAttribute('class', 'basket');
     crumbs.addCrumb('cart');
   },
   closeBasket: function() {
-    let tmp = document.querySelector('main');
-    tmp.removeAttribute('class', ' hidden');
-    tmp.setAttribute('class', 'page');
     document.querySelector('.basket').setAttribute('class', 'basket hidden');
     crumbs.removeCrumb();
+  },
+  decreaseAmount: function() {
+    let code = event.target.getAttribute('code');
+    code = Number(code);
+    for (let i in basket.shopList) {
+      if (basket.shopList[i].code === code) {
+        if (basket.shopList[i].qty === 1) {
+          basket.shopList.splice(i, 1);
+        } else
+          basket.shopList[i].qty -= 1;
+        break;
+      }
+    }
+    basket.showBasket();
+  },
+  increaseAmount: function() {
+    let code = event.target.getAttribute('code');
+    code = Number(code);
+    for (let i in basket.shopList) {
+      if (basket.shopList[i].code === code) {
+        if (basket.shopList[i].qty < basket.shopList[i].amount)
+          basket.shopList[i].qty += 1;
+        break;
+      }
+    }
+    basket.showBasket();
+  },
+  removeFromCart: function() {
+    let code = event.target.getAttribute('code');
+    code = Number(code);
+    for (let i in basket.shopList) {
+      if (basket.shopList[i].code === code) {
+        basket.shopList.splice(i, 1);
+        break;
+      }
+    }
+    basket.showBasket();
+  },
+  sort: function() {
+    let select = event.target;
 
-    const myNode = document.querySelector('.cart-items');
-    while (myNode.firstChild) {
-      myNode.removeChild(myNode.lastChild);
+    if (select.selectedIndex === 1 && !products.sortCheap) {
+      products.sortCheap = true;
+      products.sortExpen = false;
+      products.items.sort((a, b) => (a.price > b.price) ? 1 : -1);
     }
+    else if (select.selectedIndex === 2 && !products.sortExpen) {
+      products.sortCheap = false;
+      products.sortExpen = true;
+      products.items.sort((a, b) => (a.price < b.price) ? 1 : -1);
+    }
+    products.showItems();
+    document.querySelectorAll('.item_add')
+          .forEach(item => item.addEventListener('click', view.addToBasket));
   },
-  decreaseAmount: function() {
-    let code = event.target.getAttribute('code');
-    code = Number(code);
-    for (let i in basket.shopList) {
-      if (basket.shopList[i].code === code) {
-        basket.shopList[i].qty -= 1;
-        // if (basket.shopList[i].qty)
-          // delete
-        event.target.innerHTML = basket.shopList[i].qty;
+  filters: function() {
+    let arr = [],
+        boxes = document.querySelectorAll('.check-brand');
+    for (let i = 0; i < boxes.length; ++i)
+      if (boxes[i].checked) {
+        arr.push(boxes[i].getAttribute('value'));
       }
-    }
-  },
-  decreaseAmount: function() {
-    let code = event.target.getAttribute('code');
-    code = Number(code);
-    for (let i in basket.shopList) {
-      if (basket.shopList[i].code === code) {
-        basket.shopList[i].qty += 1;
-        // if (basket.shopList[i].qty)
-          // delete
-        event.target.innerHTML = basket.shopList[i].qty;
-      }
-    }
-  },
+
+    if (arr.length !== 0)
+      products.showFilteredItems(arr);
+    else
+      products.showItems();
+
+    document.querySelectorAll('.item_add')
+            .forEach(item => item.addEventListener('click', view.addToBasket));
+  }
 }
 
-// let increaseAmount = () => {
-
-// }
-// let deleteAmount = () => {
-
-// }
 
 let init = () => {
   products.showItems();
-  basket.showBasket();
 
+  document.querySelector('.sort-select').onchange = view.sort;
   document.querySelectorAll('.item_add')
           .forEach(item => item.addEventListener('click', view.addToBasket));
+  document.querySelectorAll('.check-brand')
+          .forEach(item => item.addEventListener('change', view.filters));
   document.querySelector('.header-basket').onclick = view.openBasket;
   document.querySelector('.continue').onclick = view.closeBasket;
-
-  if (document.querySelector('.cart-button-decrease'))
-    document.querySelector('.cart-button-decrease').onclick = view.decreaseAmount;
-  if (document.querySelector('.cart-button-increase'))
-    document.querySelector('.cart-button-increase').onclick = view.increaseAmount;
 }
 
 window.onload = init;
--- js/show.js
+++ js/show.js
@@ -2,37 +2,96 @@
 
 let products = {
   items: [
-    { code: 1,name: 'Jam',brand: 'St.Dalfout',taste: 'pineapple',sizeG: 284,
+    { code: 14,name: 'Jam',brand: 'J\'ELITE',taste: 'orange rum',sizeG: 220,
+      price: 8.99,amountAvailable: 1,description: 'Ukrainian candied.',
+      image: 'https://i.postimg.cc/dtpZxL0L/14.png'
+    },
+    { code: 15,name: 'Jam',brand: 'J\'ELITE',taste: 'strawberry basil tomato',sizeG: 220,
+      price: 8.99,amountAvailable: 2,description: 'Ukrainian candied.',
+      image: 'https://i.postimg.cc/wjNR8gtF/15.png'
+    },
+    { code: 1,name: 'Jam',brand: 'St.Dalfout',taste: 'blueberry',sizeG: 284,
       price: 9.99,amountAvailable: 0,description: 'Very tasty jam.',
-      image: '/images/products/pineapple.jpg'
+      image: 'https://i.postimg.cc/13SBjCXg/1.jpg'
     },
-    { code: 2,name: 'Jam',brand: 'St.Dalfout',taste: 'pineapple',sizeG: 284,
+    { code: 2,name: 'Jam',brand: 'St.Dalfout',taste: 'fig',sizeG: 284,
       price: 9.99,amountAvailable: 2,description: 'Very tasty jam.',
-      image: '/images/products/pineapple.jpg'
+      image: 'https://i.postimg.cc/4Ndp0DbF/2.jpg'
     },
-    { code: 3,name: 'Jam',brand: 'St.Dalfout',taste: 'pineapple',sizeG: 284,
-      price: 12.99,amountAvailable: 10,description: 'Very tasty jam.',
-      image: '/images/products/pineapple.jpg'
+    { code: 3,name: 'Jam',brand: 'St.Dalfout',taste: 'raspberry',sizeG: 284,
+      price: 9.99,amountAvailable: 10,description: 'Very tasty jam.',
+      image: 'https://i.postimg.cc/J42jCpp7/3.jpg'
     },
-    { code: 4,name: 'Jam',brand: 'St.Dalfout',taste: 'pineapple',sizeG: 284,
-      price: 12.99,amountAvailable: 10,description: 'Very tasty jam.',
-      image: '/images/products/pineapple.jpg'
+    { code: 4,name: 'Jam',brand: 'St.Dalfout',taste: 'orange',sizeG: 170,
+      price: 6.99,amountAvailable: 10,description: 'Very tasty jam.',
+      image: 'https://i.postimg.cc/kgRKJWHQ/4.jpg'
     },
     { code: 5,name: 'Jam',brand: 'St.Dalfout',taste: 'pineapple',sizeG: 284,
-      price: 12.99,amountAvailable: 10,description: 'Very tasty jam.',
-      image: '/images/products/pineapple.jpg'
+      price: 9.99,amountAvailable: 10,description: 'Very tasty jam.',
+      image: 'https://i.postimg.cc/6QsRyysD/5.jpg'
     },
-    { code: 6,name: 'Jam',brand: 'St.Dalfout',taste: 'pineapple',sizeG: 284,
-      price: 12.99,amountAvailable: 10,description: 'Very tasty jam.',
-      image: '/images/products/pineapple.jpg'
+    { code: 6,name: 'Jam',brand: 'Chantaine',taste: 'cranberry',sizeG: 325,
+      price: 11.99,amountAvailable: 10,description: 'Natural jam, made in France.',
+      image: 'https://i.postimg.cc/9fG9TCCz/6.jpg'
+    },
+    { code: 7,name: 'Jam',brand: 'Chantaine',taste: 'pineapple',sizeG: 325,
+      price: 11.99,amountAvailable: 10,description: 'Natural jam, made in France.',
+      image: 'https://i.postimg.cc/TwfDJF30/7.jpg'
+    },
+    { code: 8,name: 'Jam',brand: 'Chantaine',taste: 'cherry',sizeG: 325,
+      price: 11.99,amountAvailable: 10,description: 'Natural jam, made in France.',
+      image: 'https://i.postimg.cc/NMDrJDyc/8.jpg'
+    },
+    { code: 9,name: 'Jam Chyawanprash',brand: 'Dabur',taste: 'mango',sizeG: 500,
+      price: 20.99,amountAvailable: 0,description: 'Product of India for your health.',
+      image: 'https://i.postimg.cc/nrL96m7Q/9.jpg'
+    },
+    { code: 10,name: 'Jam Chyawanprash',brand: 'Dabur',taste: 'classic',sizeG: 500,
+      price: 20.99,amountAvailable: 1,description: 'Product of India for your health.',
+      image: 'https://i.postimg.cc/XN2C92SK/10.jpg'
+    },
+    { code: 11,name: 'Jam',brand: 'Bionerica',taste: 'sea buckthorn',sizeG: 200,
+      price: 2.99,amountAvailable: 10,description: 'Natural jam, made in Ukraine.',
+      image: 'https://i.postimg.cc/KvRgfnx6/11.png'
+    },
+    { code: 12,name: 'Jam Chyawanprash',brand: 'Triuga',taste: 'sugar free',sizeG: 500,
+      price: 21.99,amountAvailable: 3,description: 'Indian jam, for your health.',
+      image: 'https://i.postimg.cc/hv3QgGbZ/12.jpg'
+    },
+    { code: 13,name: 'Jam',brand: 'Bionerica',taste: 'cranberry',sizeG: 200,
+      price: 2.99,amountAvailable: 2,description: 'Natural jam, made in Ukraine.',
+      image: 'https://i.postimg.cc/KYykmwBb/13.png'
     }
   ],
+  sortCheap: false,
+  sortExpen: false,
+  showFilteredItems: function(arr) {
+    let elem = document.querySelector('.container');
+
+    if (elem.firstChild)
+      while (elem.firstChild)
+        elem.removeChild(elem.lastChild);
+
+    for (let i in this.items) {
+      for (let j = 0; j < arr.length; ++j) {
+        if (this.items[i].brand === arr[j]) {
+          this.showItem(elem, this.items[i]);
+          break;
+        }
+      }
+    }
+  },
   showItems: function() {
     let elem = document.querySelector('.container');
+
+    if (elem.firstChild)
+      while (elem.firstChild)
+        elem.removeChild(elem.lastChild);
+
     for (let i in this.items)
-      this.showItem(elem, this.items[i], i);
+      this.showItem(elem, this.items[i]);
   },
-  showItem: function(elem, item, i) {
+  showItem: function(elem, item) {
     let node, shelf, p, p1;
 
     node = document.createElement('div');
@@ -62,14 +121,17 @@ let products = {
       p1.innerHTML = '\$' + item.price;
       p.appendChild(p1);
 
+      p1 = document.createElement('input');
+      p1.setAttribute('code', item.code);
+      p1.setAttribute('value', 'Add to Cart');
       if (item.amountAvailable !== 0) {
-        p1 = document.createElement('a');
-        p1.setAttribute('href', '#');
-        p1.setAttribute('data', i);
+        p1.setAttribute('type', 'button');
         p1.className = 'item_add button';
-        p1.innerHTML = 'Add to Cart';
-        p.appendChild(p1);
       }
+      else
+        p1.className = 'button-empty';
+      p.appendChild(p1);
+
     shelf.appendChild(p);
 
     p = document.createElement('span');
@@ -89,9 +151,6 @@ let products = {
   }
 }
 
-// <div class="qty">quantity</div><input type="text" value="1" class="item_Quantity">
-// <div data-location="/checkout/" class="button">Buy</div>
-
 let basket = {
   total: 0,
   shopList: [],
@@ -107,9 +166,8 @@ let basket = {
     for (let i in this.shopList) {
       this.showBasketItem(elem, this.shopList[i], i);
     }
-    document.querySelector('.cart-numTotal').innerHTML = '\$' + this.total;
+    document.querySelector('.cart-numTotal').innerHTML = '\$' + this.total.toFixed(2);
   },
-  // name, price, qty (+-), subtotal, remove button
   showBasketItem: function(elem, shopList, i) {
     let node, p, p1, p2;
 
@@ -129,24 +187,29 @@ let basket = {
 
         p2 = document.createElement('a');
         p2.setAttribute('href', '#');
+        p2.setAttribute('code', shopList.code);
+        p2.setAttribute('onclick', 'view.decreaseAmount()');
         p2.className = 'cart-button-decrease';
         p2.innerHTML = '-';
         p.appendChild(p2);
 
         p2 = document.createElement('span');
-        p2.setAttribute('code', shopList.code);
         p2.className = 'item-qty';
         p2.innerHTML = shopList.qty;
         p.appendChild(p2);
 
         p2 = document.createElement('a');
         p2.setAttribute('href', '#');
+        p2.setAttribute('code', shopList.code);
+        p2.setAttribute('onclick', 'view.increaseAmount()');
         p2.className = 'cart-button-increase';
         p2.innerHTML = '+';
         p.appendChild(p2);
 
         p2 = document.createElement('a');
         p2.setAttribute('href', '#');
+        p2.setAttribute('code', shopList.code);
+        p2.setAttribute('onclick', 'view.removeFromCart()');
         p2.setAttribute('itemInBasket', i);
         p2.className = 'cart-remove';
         p2.innerHTML = '+';
